c6fe113431099c0438c8b6955be1d7b1e2b590e8
sophlouiseh <s.l.higgins92@gmail.com>
2019-05-06 10:46:19 +0100

    Merchant controller

--- GeoPay_API/GeoPay_API/Repos/MerchantRepo.cs
+++ GeoPay_API/GeoPay_API/Repos/MerchantRepo.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using Dapper;
 using GeoPay_API.Models;
@@ -11,10 +12,14 @@ namespace GeoPay_API.Repos
     {
         public IEnumerable<Merchant> GetMerchants()
         {
-            var dbFilePath = "../ecb.db";
+            var dbFilePath = "../../ecb.db";
+            if (!File.Exists(dbFilePath))
+            {
+                throw new Exception("Database file not found");
+            }
             var _dbConnection = new SQLiteConnection(string.Format("Data Source={0};Version=3;", dbFilePath));
             _dbConnection.Open();
-            return _dbConnection.Query<Merchant>("Select * From Merchant").ToList();
+            return _dbConnection.Query<Merchant>("SELECT Id, Name, AccountNumber FROM Merchant").ToList();
         }
     }
 }
