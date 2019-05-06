42e183590c111d2de6070b631d41f806c11c668a
Keah Peters <keahpeters@hotmail.co.uk>
2019-05-06 10:01:01 +0100

    Configured swagger

--- GeoPay_API/GeoPay_API/BankService.cs
+++ GeoPay_API/GeoPay_API/BankService.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GeoPay_API.Models;
 
 namespace GeoPay_API
 {
-    public class BankService
+    public class BankService : IBankService
     {
+        public PaymentStatus ExecutePayment(string transactionId) => throw new NotImplementedException();
+
+        public PaymentStatus GetPaymentStatus(string transactionId) => throw new NotImplementedException();
+
+        public PaymentStatus RegisterPayment(Payment payment) => throw new NotImplementedException();
     }
 }
