fe94f39b785ca857f8f488b8a47121500b171db6
Keah Peters <keahpeters@hotmail.co.uk>
2019-05-06 10:47:19 +0100

    Added DbConnectionFactory

---
+++ GeoPay_API/GeoPay_API/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace GeoPay_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        public string PhoneId { get; set; }
+    }
+}
