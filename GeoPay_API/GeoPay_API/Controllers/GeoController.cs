752302631e830acf5bf56066203477bf7be82eb1
TheLastColonial <alex.aim88@gmail.com>
2019-05-06 09:10:05 +0100

    Added Libs

---
+++ GeoPay_API/GeoPay_API/Controllers/GeoController.cs
@@ -0,0 +1,12 @@
+﻿using GeoPay_API.Repos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoPay_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoController : ControllerBase
+    {
+        private IGeoRepo GeoRepo { get; set; }
+    }
+}
