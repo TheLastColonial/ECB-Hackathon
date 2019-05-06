2decf66dcc699d7c162a557c01e5cc93975b1f5e
Keah Peters <keahpeters@hotmail.co.uk>
2019-05-06 09:45:22 +0100

    Added data script

---
+++ GeoPay_API/GeoPay_API/Models/Payment.cs
@@ -0,0 +1,15 @@
+﻿namespace GeoPay_API.Models
+{
+    public class Payment
+    {
+        public string InitiatingPartyAccountNumber { get; set; }
+
+        public string CounterPartyAccountNumber { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string CounterpartyName { get; set; }
+
+        public string RemittanceInfo { get; set; }
+    }
+}
---
+++ GeoPay_API/GeoPay_API/Models/PaymentStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace GeoPay_API.Models
+{
+    public class PaymentStatus
+    {
+        public string AccountNumber { get; set; }
+
+        public string TransactionId { get; set; }
+
+        public string Status { get; set; }
+    }
+}
