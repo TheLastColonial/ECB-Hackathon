12a8130f723be8376e480b38511edddef658c18f
Keah Peters <keahpeters@hotmail.co.uk>
2019-05-06 16:10:00 +0100

    Database update

--- data_script.sql
+++ data_script.sql
@@ -4,13 +4,8 @@ insert into user values(null, null, 'Alex', 'Alex', 'Woodward', '00000001', '1')
 
 insert into merchant values(null, 'Test merchant', '00000007');
 
-insert into location values(null, 'GoogleReference', 1.0, 1.0, 1, (SELECT id FROM merchant));
+insert into location values(null, 'GoogleReference', 1.0, 1.0, 1, (SELECT id FROM merchant), 4.99);
 
 insert into subscription values(null, (SELECT id FROM user), (select id FROM merchant));
 
 COMMIT;
-
-
-
-select * from merchant;
-select * from location;
