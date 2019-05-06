767ac1415b70afaf543a630904fce147bda5bbba
Keah Peters <keahpeters@hotmail.co.uk>
2019-05-06 09:04:51 +0100

    Added BankApi

---
+++ BankApi/BankApi/Models/Payment.cs
@@ -0,0 +1,15 @@
+﻿namespace FakeBankService.Models
+{
+    public class Payment
+    {
+        public string InitiatingPartyAccountNumber { get; set; }
+
+        public string CounterPartyAccountNumber { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string CounterpartyName { get; set; }
+
+        public string RemittanceInfo { get; set; }
+    }
+}
---
+++ BankApi/BankApi/Models/PaymentStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace FakeBankService.Models
+{
+    public class PaymentStatus
+    {
+        public string AccountNumber { get; set; }
+
+        public string TransactionId { get; set; }
+
+        public string Status { get; set; }
+    }
+}
